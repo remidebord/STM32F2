2aa307ba7ac896a606763308d3e6c2a7c6d7cb06
remidebord <remidebord@hotmail.fr>
2015-07-27 13:38:55 +0200

    Keil 5.15 + CMSIS

--- template/src/main.c
+++ template/src/main.c
@@ -1,31 +1,28 @@
 #include "main.h"
+	
+DigitalOut led1(PC8);
+DigitalOut led2(PC9);
+	
+// DigitalIn pushButton(PA0);
+InterruptIn pushButton(PA0);
 
-#define FREQUENCY 10000 // 1kHz
-//#define FREQUENCY 50 // 50Hz
-
-PwmOut pwm(PA9, FREQUENCY, TIM1_CH2);
-//PwmOut pwm(PA8, FREQUENCY, TIM1_CH1);
-//PwmOut pwm(PA0, FREQUENCY, TIM2_CH1);
-//PwmOut pwm(PA3, FREQUENCY, TIM2_CH4);
-//PwmOut pwm(PB0, FREQUENCY, TIM3_CH3);
-//PwmOut pwm(PB7, FREQUENCY, TIM4_CH2);
-//PwmOut pwm(PA0, FREQUENCY, TIM5_CH1);
-//PwmOut pwm(PC9, FREQUENCY, TIM8_CH4);
-//PwmOut pwm(PA3, FREQUENCY, TIM9_CH2);
-//PwmOut pwm(PB8, FREQUENCY, TIM10_CH1);
-//PwmOut pwm(PB9, FREQUENCY, TIM11_CH1);
-//PwmOut pwm(PB15, FREQUENCY, TIM12_CH2);
-//PwmOut pwm(PA6, FREQUENCY, TIM13_CH1);
-//PwmOut pwm(PA7, FREQUENCY, TIM14_CH1);
+void Push(void)
+{
+	led2 = !led2;
+}
 
 int main(void)
 {
-  Systick_Init();
+	Systick_Init();
 	
-	// Set duty cycle to 20%
-	pwm = 0.2;
+	pushButton.rise(&Push);
 	
   while (1)
   {
+		led1 = !led1;
+		Delay(100);
+		
+// 		if(pushButton) led2 = 1;
+// 		else led2 = 0;
   }
 }
